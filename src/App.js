76aaeadd7895565e37a62ae4f8fbc35451b0809b
nazmul162001 <nazmulprime162001@gmail.com>
2022-03-15 22:44:30 +0600

    add props & components

--- src/App.js
+++ src/App.js
@@ -16,19 +16,36 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
+      <Person name="Rubel" nayeka = "Mousumi"></Person>
+      <Person name = "Bapparaj" nayeka = "cheka"></Person>
+      <Person name = "Akash" job = "Web Developement" salary = "2000"></Person>
+
+      <h2>New Components</h2>
+      <Friend name = "Nazmul" skill = "Web Developement" salary = "25000"></Friend>
+      <Friend name = "Akash Mahmud" skill = "React" salary = "30000"></Friend>
+
     </div>
   )
 }
 
-function Person () {
+function Person (props) {
   return (
     <div className="person">
-      <h1>Sakib Al Hasan</h1>
-      <p>Location: Bangladesh</p>
+      <h1>{props.name}</h1>
+      <p>Nayeka: {props.nayeka}</p>
+      <p>Job: {props.job}</p>
+      <p>Salary: {props.salary}</p>
+    </div>
+  )
+}
+
+function Friend(props){
+  return(
+    <div className='container'>
+      <h3>Name: {props.name}</h3>
+      <p>Skill: {props.skill}</p>
+      <p>Salary: {props.salary}</p>
+
     </div>
   )
 }
